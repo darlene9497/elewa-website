2731fc18349ec8fe846b27c03654be79408f5cd8
darlene9497 <darlene.nandabi@student.moringaschool.com>
2023-08-21 19:12:28 +0300

    :sparkles: generated elewa website price item card component

---
+++ libs/elements/cards/src/lib/elewa-website-price-item-card/elewa-website-price-item-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'elewa-website-elewa-website-price-item-card',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './elewa-website-price-item-card.component.html',
+  styleUrls: ['./elewa-website-price-item-card.component.scss'],
+})
+export class ElewaWebsitePriceItemCardComponent {}
