864d0bbe85c77a6a1d77e0dd88e60dad49ee24c2
darlene9497 <darlene.nandabi@student.moringaschool.com>
2023-09-02 10:56:36 +0300

    :lipstick: added the text section at the hero

--- libs/features/pages/content-development/src/lib/main/components/content-dev-hero-section/content-dev-hero-section.component.ts
+++ libs/features/pages/content-development/src/lib/main/components/content-dev-hero-section/content-dev-hero-section.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { HeroText } from '@elewa-website/models/schema/ui/texts';
 
 @Component({
   selector: 'elewa-website-content-dev-hero-section',
   templateUrl: './content-dev-hero-section.component.html',
   styleUrls: ['./content-dev-hero-section.component.scss'],
 })
-export class ContentDevHeroSectionComponent {}
+export class ContentDevHeroSectionComponent {
+  textData: HeroText = {
+    title: 'Content development',
+    subtitle: '',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor dolorem ipsum quia dolor sit Eos qui ratione voluptatem sequi nesciunt.',
+  };
+}
