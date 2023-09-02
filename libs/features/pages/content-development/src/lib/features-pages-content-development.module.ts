cb9a9a58829b130cb5783e042d3d81df05f4612c
darlene9497 <darlene.nandabi@student.moringaschool.com>
2023-09-02 10:26:16 +0300

    :sparkles: generated the content dev hero section component

--- libs/features/pages/content-development/src/lib/features-pages-content-development.module.ts
+++ libs/features/pages/content-development/src/lib/features-pages-content-development.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ContentDevelopmentPageComponent } from './main/content-development-page/content-development-page.component';
+import { ContentDevHeroSectionComponent } from './main/components/content-dev-hero-section/content-dev-hero-section.component';
+import { AppHeaderModule } from '@elewa-website/elements/layout/header';
 
 import { ContentDevelopmentPageRoutingModule } from './content-development.routing';
 
 @NgModule({
-  imports: [CommonModule, ContentDevelopmentPageRoutingModule],
-  declarations: [ContentDevelopmentPageComponent],
-  exports: [ContentDevelopmentPageComponent]
+  imports: [CommonModule, ContentDevelopmentPageRoutingModule, AppHeaderModule],
+  declarations: [ContentDevelopmentPageComponent, ContentDevHeroSectionComponent],
+  exports: [ContentDevelopmentPageComponent, ContentDevHeroSectionComponent]
 })
 export class ContentDevelopmentModule {}
