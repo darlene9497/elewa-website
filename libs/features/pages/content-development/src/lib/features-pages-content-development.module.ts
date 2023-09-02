3596161e8c72267f209223d697052f3355c6e385
darlene9497 <darlene.nandabi@student.moringaschool.com>
2023-09-02 10:33:18 +0300

    :art: imported texts and buttons module to the content development module

--- libs/features/pages/content-development/src/lib/features-pages-content-development.module.ts
+++ libs/features/pages/content-development/src/lib/features-pages-content-development.module.ts
@@ -4,11 +4,13 @@ import { CommonModule } from '@angular/common';
 import { ContentDevelopmentPageComponent } from './main/content-development-page/content-development-page.component';
 import { ContentDevHeroSectionComponent } from './main/components/content-dev-hero-section/content-dev-hero-section.component';
 import { AppHeaderModule } from '@elewa-website/elements/layout/header';
+import { TextsModule } from '@elewa-website/elements/layout/texts';
+import { ButtonsModule } from '@elewa-website/elements/layout/buttons';
 
 import { ContentDevelopmentPageRoutingModule } from './content-development.routing';
 
 @NgModule({
-  imports: [CommonModule, ContentDevelopmentPageRoutingModule, AppHeaderModule],
+  imports: [CommonModule, ContentDevelopmentPageRoutingModule, AppHeaderModule, TextsModule, ButtonsModule],
   declarations: [ContentDevelopmentPageComponent, ContentDevHeroSectionComponent],
   exports: [ContentDevelopmentPageComponent, ContentDevHeroSectionComponent]
 })
